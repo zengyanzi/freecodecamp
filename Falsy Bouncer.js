6ab9ac829539e8b5bef1f25f28519c6cf39b57d5
zhen zeng <zengyanzi@users.noreply.github.com>
2017-03-03 11:55:34 +1300

    Falsy Bouncer

---
+++ Falsy Bouncer.js
@@ -0,0 +1,8 @@
+//Remove all falsy values from an array.
+
+//Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+function bouncer(arr) {
+ return arr.filter(Boolean);
+}
+bouncer([7, "ate", "", false, 9]);
