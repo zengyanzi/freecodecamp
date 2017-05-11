4d090a90736c68192b631829aa431472848c72ed
zhen zeng <zengyanzi@users.noreply.github.com>
2017-05-12 10:23:49 +1200

    Update Pig Latin.js

--- Pig Latin.js
+++ Pig Latin.js
@@ -14,3 +14,8 @@ function translatePigLatin(str) {
 }
 
 translatePigLatin("consonant");
+
+//The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+
+var a = [2, 9, 9]; 
+a.indexOf(2); // 0 
